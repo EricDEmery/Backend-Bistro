0ccd94c95ab6cc5b6b6c34c353a6ee75cf970515
Eric Emery <ericemery099@gmail.com>
2023-07-28 10:08:25 +0000

    update

--- backendbistro/menu/views.py
+++ backendbistro/menu/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from django.http import HttpResponse
+from .models import Menu_Item, Ingredient
+from django.core import serializers
+from django.forms.models import model_to_dict
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def full_menu(request):
+    try:
+        data = [i.json() for i in Menu_Item.objects.all()]      #Calling json method on Menu_Item and looping through
+        return HttpResponse(json.dumps(data), content_type="application/json")  #Returning http response. json.dumps parses to jason
+        #Checking if Menu_item model does not exist and raises error if true
+    except Menu_Item.DoesNotExist:
+        errmsg = "Does not exist"
+        raise err(errmsg)
